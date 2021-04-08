969cec04b0dcd7a1ffb7307614b180e6e49ae36e
Ubuntu <ubuntu@ip-172-20-0-46.ap-south-1.compute.internal>
2021-04-08 12:19:03 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0bcf5425cdc1d8a85"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
