8caf0f96287a8c84d4502f5aee1d3b42782e24e0
Yukesh <61724471+ykv97@users.noreply.github.com>
2021-04-17 21:17:51 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 3.0"
     }
-  required_version = ">= 0.14.0"
-}
+  }
