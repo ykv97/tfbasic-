b6da8bc9f7edd546e4a5933734ab3254b02cc374
Yukesh <61724471+ykv97@users.noreply.github.com>
2021-04-17 20:55:53 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,22 +1,57 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
 provider "aws" {
-  profile = "default"
-  region  = "ap-south-1"
+  region = var.aws_region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0bcf5425cdc1d8a85"
-  instance_type = "t2.micro"
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = var.instance_name
+    Name = var.vpc_name
   }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.vpc_id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-south-1a"
+   tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.1.0.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "public"
+  }
+}
+
+
+resource "aws_route_table" "internet" {
+  vpc_id = aws_vpc.example.id
+  tags = {
+    Name = "internet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "gateway"
+  }
+}
+
+
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.internet.id
+}
+
+resource "aws_route" "r" {
+  route_table_id = aws_route_table.internet.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "ExampleInstance"
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "testvpc"
 }
