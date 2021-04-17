cde3bb6aae73ee29cf7c0dd779644ff895276ae1
Yukesh <61724471+ykv97@users.noreply.github.com>
2021-04-17 21:53:54 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
 
 provider "aws" {
   region = var.aws_region
@@ -30,7 +22,7 @@ resource "aws_subnet" "private" {
 
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.1.0.0/24"
+  cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch = "true"
   tags = {
